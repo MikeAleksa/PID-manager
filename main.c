0829e2e26790493f9257c0402537e6c67dc9046e
mike <mtaleksa1@gmail.com>
2019-02-15 15:58:50 -0500

    change malloc() to calloc() to fix allocation size bug and remove need for initializing the pid_map

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "pid_manager.h"
 
-// This is a driver program to test the pid_manager
+// This is a driver program to test the pid_manager program
 
 int main() {
 
@@ -11,9 +11,10 @@ int main() {
 
     int map_allocated = allocate_map();
     if (map_allocated == -1)
-        return (1);
+        return (-1);
 
-    for (int i = 0; i <= 4810; i++) {
+    // allocate all available pids with no print statements
+    for (int i = 0; i <= 5001; i++) {
         int pid = allocate_pid();
         if (pid == -1)
             printf("allocation failed: no pids available\n");
@@ -21,16 +22,22 @@ int main() {
             printf("allocated pid %i\n", pid);
     }
 
-    for (int j = 0; j < 100; j++) {
-        for (int i = 0; i < 10; i++) {
+    // show release_pid and allocate_pid working with print statements
+    for (int j = 0; j < 3; j++) {
+        // release pid between 300 and 5000
+        for (int i = 0; i < 5; i++) {
             int pid = rand() % 4700 + 300;
             release_pid(pid);
             printf("release pid %i\n", pid);
         }
 
-        for (int i = 0; i < 10; i++) {
+        // try to allocate one more pid than available
+        for (int i = 0; i < 6; i++) {
             int pid = allocate_pid();
-            printf("allocated pid %i\n", pid);
+            if (pid == -1)
+                printf("allocation failed: no pids available\n");
+            else
+                printf("allocated pid %i\n", pid);
         }
     }
 
--- pid_manager.c
+++ pid_manager.c
@@ -1,41 +1,40 @@
-#include <stdlib.h>         /* for malloc() */
+#include <stdlib.h>         /* for calloc() */
 #include <limits.h>         /* for CHAR_BIT */
 #include "pid_manager.h"
 
 #define MIN_PID 300
 #define MAX_PID 5000
-const unsigned char fullWord = ~((unsigned char) 0);     /* used to check for full words in the bitmap */
 unsigned char *pid_map = NULL;
 
 /* allocate a bitmap using an array of unsigned chars and assign the bitmap to the global pointer 'pid_map'
  * return values are 1 (successful allocation) and -1 (unsuccessful allocation)
- *
- * this bitmap allocates space for pids below MIN_PID under the assumption they will be used as
- * pids reserved for special processes, similar to the RESERVED_PIDS in the linux kernel's pid.c */
+ *  note: this bitmap allocates space for pids below MIN_PID under the assumption they will be used as
+ *  pids reserved for special processes, similar to the RESERVED_PIDS in the linux kernel's pid.c */
 int allocate_map(void) {
-    // if MAX_PID does not divide evenly by CHAR_BIT, include one extra word in pid_map
-    unsigned char extraWord = 0;
-    if (MAX_PID % CHAR_BIT != 0)
-        extraWord = 1;
-    // allocate a pid_map of unsigned char * and return 0 if allocation fails, or set all words
-    //  in pid_map to 0 and return 1
-    pid_map = (unsigned char *) malloc(MAX_PID / CHAR_BIT + extraWord);
-    if (pid_map == NULL) return -1;
-    for (int i = 0; i < MAX_PID / CHAR_BIT; i++) {
-        pid_map[i] = 0;
+    // if MAX_PID does not divide evenly by CHAR_BIT, include one extra word in pid_map for the remainder
+    size_t words = MAX_PID / CHAR_BIT;
+    if (MAX_PID % CHAR_BIT != 0) {
+        words += 1;
     }
-    return 1;
+    // allocate a pid_map of unsigned char * and return -1 if allocation fails or 1 if allocation succeeds
+    //  note: calloc() initializes all values to 0
+    pid_map = (unsigned char *) calloc(words, sizeof(char));
+    if (pid_map == NULL) return -1;
+    else return 1;
 }
 
 /* search for first free pid between MIN_PID and MAX_PID and allocate that pid
  * returns the pid number, or will return -1 if all pids are already allocated */
 int allocate_pid(void) {
     // search for first free pid between MIN_PID and MAX_PID and allocate
-    for (int bit = MIN_PID; bit <= MAX_PID; bit++) {
-        // if statement checks if (a bit in the pid_map & 1) is false, i.e. the bit in pid_map = 0
-        if (!(pid_map[bit / CHAR_BIT] & (1 << (bit % CHAR_BIT)))) {
-            pid_map[bit / CHAR_BIT] |= (1 << (bit % CHAR_BIT));
-            return bit;
+    for (int pid = MIN_PID; pid <= MAX_PID; pid++) {
+        // do nothing if (a pid in the pid_map & 1) is true, i.e. the pid in pid_map = 1
+        // else, allocate and return pid
+        if (pid_map[pid / CHAR_BIT] & (1 << (pid % CHAR_BIT)))
+            continue;
+        else {
+            pid_map[pid / CHAR_BIT] |= (1 << (pid % CHAR_BIT));
+            return pid;
         }
     }
     return -1;
