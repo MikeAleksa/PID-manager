95cd06ba025d972ac6bf90742e0f6f94e240e01c
mike <mtaleksa1@gmail.com>
2019-02-26 23:16:38 -0500

    set up project for hw2 and start rewriting main.c for multithreaded process simulation

--- main.c
+++ main.c
@@ -1,49 +1,54 @@
 /* Written by Michael Aleksa for CISC-3320 EM6 */
 
-#include <stdio.h>        /* for printf() */
-#include <time.h>         /* for time() to seed srand() */
-#include <stdlib.h>       /* for rand() */
+#include <stdio.h>          /* for printf() */
+#include <time.h>           /* for time() to seed srand() */
+#include <stdlib.h>         /* for rand() */
+#include <unistd.h>         /* for sleep() */
 #include "pid_manager.h"
 
-/* This is a driver program to test the pid_manager program */
+void create_threads();
+
+void simulate_process();
 
 int main() {
     srand(time(NULL));
 
+    /* allocate pid map */
     int map_allocated = allocate_map();
     if (map_allocated == -1) {
         printf("pid_map failed to allocate (-1)");
-        return (-1);
+        exit(-1);
     }
 
-    printf("allocating pids from %i to %i...\n", MIN_PID, MAX_PID);
-    /* allocate all available pids with no print statements */
-    for (int i = MIN_PID; i <= MAX_PID; i++) {
-        allocate_pid();
-    }
-    printf("all pids allocated\n\n");
+    /* create 100 threads */
+    create_threads();
 
-    /* show release_pid and allocate_pid working with print statements */
-    for (int j = 0; j < 3; j++) {
-        for (int i = 0; i < 5; i++) {
-            /* release a random pid between MIN_PID and MAX_PID */
-            int pid = MIN_PID + rand() % (MAX_PID + 1 - MIN_PID);
-            release_pid(pid);
-            printf("release pid %i\n", pid);
-        }
-        printf("\n");
+    /* simulate processes on threads */
 
-        for (int i = 0; i < 6; i++) {
-            /* try to allocate a pid */
-            int pid = allocate_pid();
-            if (pid == -1)
-                printf("allocation failed: no pids available\n");
-            else
-                printf("allocated pid %i\n", pid);
-        }
-        printf("\n");
-    }
 
+    /* deallocate pid map */
     deallocate_map();
     return 0;
+}
+
+
+void create_threads() {
+
+}
+
+void simulate_process() {
+    /* try to allocate a pid and return if unable */
+    int pid = allocate_pid();
+    if (pid == -1) {
+        printf("allocation failed: no pids available\n");
+        return;
+    } else {
+        printf("allocated pid %i\n", pid);
+    }
+    /* sleep for a random amount of time between 5 and 30 seconds */
+    unsigned int sleep_time = 5u + (rand() % 25u);
+    sleep(sleep_time);
+    /* release pid */
+    release_pid(pid);
+    printf("release pid %i\n", pid);
 }
